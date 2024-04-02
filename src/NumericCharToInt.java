672892d2d522f963f1a8301fd8d9027bb9316230
aditya <adityaatri9@gmail.com>
2024-04-02 11:56:17 +0530

    commit

---
+++ src/NumericCharToInt.java
@@ -0,0 +1,15 @@
+
+public class NumericCharToInt 
+{
+	public static int num(char ch)
+	{
+		int n = ch - 48;
+		return n;
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println(num('7'));
+		
+	}
+
+}
