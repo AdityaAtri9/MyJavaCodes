3ef34a5b4c2ad86d2fff270bed2f30b9aaa9af4d
aditya <adityaatri9@gmail.com>
2024-04-02 11:59:38 +0530

    commit

--- src/NumericCharToInt.java
+++ src/NumericCharToInt.java
@@ -3,7 +3,7 @@ public class NumericCharToInt
 {
 	public static int num(char ch)
 	{
-		int n = ch - 48;
+		int n = ch - 48; // 48 is the unicode of '0'
 		return n;
 	}
 	public static void main(String[] args) 
